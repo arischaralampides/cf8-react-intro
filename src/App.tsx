04ab466708a901f077d8defed2c9f122a1128c4f
Aris Charalampidis <aris_charalampides@hotmail.com>
2025-10-05 20:42:33 +0300

    feat: add class and functional components

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 // import ViteIntro from "./components/ViteIntro/ViteIntro.tsx";
-
-import ClassComponent from "./components/ClassComponent.tsx";
-import FunctionalComponent from "./components/FunctionalComponent.tsx";
-import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+//import ClassComponent from "./components/ClassComponent.tsx";
+//import FunctionalComponent from "./components/FunctionalComponent.tsx";
+//import ArrowFunctionalComponent from "./components/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./components/ArrowFunctionalComponentWithProps.tsx";
 
 
 function App() {
@@ -10,10 +10,12 @@ function App() {
 
     return (
         <>
-            {/*<ViteIntro />*/}
-            <ClassComponent/>
-            <FunctionalComponent/>
-            <ArrowFunctionalComponent/>
+            {/*/!*<ViteIntro />*!/*/}
+            {/*<ClassComponent/>*/}
+            {/*<FunctionalComponent/>*/}
+            {/*<ArrowFunctionalComponent/>*/}
+            <ArrowFunctionalComponentWithProps title="Is an Arrow Functional Component With Props" />
+            <ArrowFunctionalComponentWithProps title="2nd title" />
         </>
     )
 }
